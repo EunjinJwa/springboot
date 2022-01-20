32c022d5e6c2b4b6a43cd1cea27fa230e59efe1c
kassyJwa <kassy.jwa@nsuslab.com>
2022-01-20 22:25:49 +0900

    sample project 생성

---
+++ sample-project/src/main/java/jinny/springboot/sample/project/WorkerApplication.java
@@ -0,0 +1,16 @@
+package jinny.springboot.sample.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class WorkerApplication {
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(WorkerApplication.class);
+
+        System.out.println("run Application ..!!! ");
+        application.run(args);
+    }
+}
